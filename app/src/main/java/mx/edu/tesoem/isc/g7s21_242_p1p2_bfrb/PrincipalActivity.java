cb2ef3774cba2363452235b43520695894bdff84
BrayanRodriguez020 <brayanfernando.rb@gmail.com>
2024-09-19 18:30:35 -0600

    Cambio de pantalla

---
+++ app/src/main/java/mx/edu/tesoem/isc/g7s21_242_p1p2_bfrb/PrincipalActivity.java
@@ -0,0 +1,20 @@
+package mx.edu.tesoem.isc.g7s21_242_p1p2_bfrb;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_principal);
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "G7S21_242_p1p2_bfrb"
+include(":app")
+ 
